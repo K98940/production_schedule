073351a6daf6e50804bcbb4328da60bb6a193a98
k.shumkin <pv1xxb@yandex.ru>
2025-06-22 21:44:19 +0600

    Revert "Добавлены поддержка устаревших браузеров."
    This reverts commit 3da47ca4a5180afa939ae941957d8fc129b1fa9e.

--- vite.config.ts
+++ vite.config.ts
@@ -5,45 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss(),
-    legacy({
-      targets: [
-        'Firefox >= 60',
-        'Chrome >= 67',
-        'Safari >= 11',
-        'Edge >= 79'
-      ],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-      renderLegacyChunks: true,
-      polyfills: [
-        'es.symbol',
-        'es.promise',
-        'es.promise.finally',
-        'es/map',
-        'es/set',
-        'es.array.filter',
-        'es.array.for-each',
-        'es.array.flat-map',
-        'es.object.define-properties',
-        'es.object.define-property',
-        'es.object.get-own-property-descriptor',
-        'es.object.get-own-property-descriptors',
-        'es.object.keys',
-        'es.object.to-string',
-        'web.dom-collections.for-each',
-        'esnext.global-this',
-        'esnext.string.match-all'
-      ]
-    })
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -66,11 +31,5 @@ export default defineConfig({
         },
       },
     },
-  },
-  optimizeDeps: {
-    include: ['vue', 'pinia', 'primevue'],
-  },
-  esbuild: {
-    target: 'es2017',
   },
 })
