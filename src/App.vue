6a9759645aeb4b9486f0591e9a08ee30879369c3
k.shumkin <pv1xxb@yandex.ru>
2025-06-24 12:15:58 +0600

    Добавлено отображение панели информации в компоненте App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import ViewTopMenu from './views/ViewTopMenu.vue'
 import ViewCalendar from './views/ViewCalendar.vue'
+import ViewInfoPanel from './views/ViewInfoPanel.vue'
 import { handleMouseUp } from './handlers/handleMouseUp'
 import { handleMouseMove } from './handlers/handleMouseMove'
-//FIX 5. Интервал один день. Иногда ширина контейнера превышает ширину окна,
-// примерно на ширину левой панели. Смотреть условие ширина колонки < чего-то там.
-// В калькуляции координат в календаре.
+
 </script>
 
 <template>
@@ -16,6 +15,7 @@ import { handleMouseMove } from './handlers/handleMouseMove'
     @mouseleave="handleMouseUp"
   >
     <ViewTopMenu />
+    <ViewInfoPanel />
     <ViewCalendar />
   </div>
 </template>
